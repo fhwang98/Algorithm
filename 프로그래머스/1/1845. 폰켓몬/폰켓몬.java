31fed6931f44a4219e062b8ad93fb4a0e53ad5bc
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2024-07-08 21:03:33 +0900

    [level 1] Title: 폰켓몬, Time: 3.39 ms, Memory: 82.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+
+class Solution {
+    public int solution(int[] nums) {
+        
+        int answer = 0;
+        HashSet<Integer> set = new HashSet<Integer>();
+        
+        for (int n : nums) {
+            set.add(n);
+        }
+        
+        answer = set.size() >= nums.length / 2 ? nums.length / 2 : set.size();
+        
+        return answer;
+    }
+}
