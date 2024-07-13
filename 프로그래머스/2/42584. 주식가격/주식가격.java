43cb56cdd79654164a6292bd0a7d1f273f3f4be1
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2024-07-13 17:59:09 +0900

    [level 2] Title: 주식가격, Time: 24.35 ms, Memory: 69.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,16 +1,11 @@
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        int cur = 0;
-        int sec = 0;
         for (int i = 0; i < prices.length - 1; i++) {
-            cur = prices[i];
-            sec = 0;
             for (int j = i + 1; j < prices.length; j++) {
-                sec++;
-                if (prices[j] < cur) break;
+                answer[i]++;
+                if (prices[i] > prices[j]) break;
             }
-            answer[i] = sec;
         }
         return answer;
     }
