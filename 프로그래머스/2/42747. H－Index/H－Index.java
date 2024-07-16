e5964f68f24c0bfa1a2e68a2fc43204acdfb8e3e
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2024-07-16 21:32:55 +0900

    [level 2] Title: H-Index, Time: 1.03 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -4,10 +4,11 @@ class Solution {
     public int solution(int[] citations) {
         
         int answer = 0;
+        int h = 0;
         Arrays.sort(citations);
         
         for (int i = 0; i < citations.length; i++) {
-            int h = citations.length - i;
+            h = citations.length - i;
             if (citations[i] >= h) {
                 answer = h;
                 break;
