fda784ba0150135176d433996e5ed77180755a8e
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2024-07-17 12:55:36 +0900

    [level 2] Title: 소수 찾기, Time: 30.46 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -2,33 +2,29 @@ import java.util.HashSet;
 
 class Solution {
     
-    HashSet<Integer> set = new HashSet<>();
-
+    private HashSet<Integer> set;
+    private int cur;
+    
     public int solution(String numbers) {
-        // 재귀를 이용해서 모든 조합 만들기
+        set = new HashSet<Integer>();
         rec("", numbers);
-
         return set.size();
     }
-
-    // 재귀를 이용한 조합 메소드
-    public void rec (String temp, String n) {
+    
+    public void rec(String s, String numbers) {
         
-        // 현재 조합을 set에 추가
-        if(!temp.equals("")) {
-            int cur = Integer.parseInt(temp);
+        if (!s.equals("")) {
+            cur = Integer.parseInt(s);
             if (isPrime(cur)) set.add(cur);
         }
-
-        // 나머지 숫자를 더해 조합 만들기
-        for (int i = 0; i < n.length(); i++) {
-            // 재귀 호출
-            rec(temp + n.charAt(i), n.substring(0, i) + n.substring(i + 1));
+        
+        for (int i = 0; i < numbers.length(); i++) {
+            rec(s + numbers.charAt(i), numbers.substring(0, i) + numbers.substring(i + 1));
         }
     }
     
     public boolean isPrime(int n) {
-        if (n <= 1) return false;
+        if (n < 2) return false;
         for (int i = 2; i <= n / i; i++) {
             if (n % i == 0) return false;
         }
