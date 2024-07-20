8df0f639d5dce0096e4145e3f9e1d0a22927d600
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2024-07-20 11:25:48 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -6,5 +6,5 @@ SELECT board_id, writer_id, title, price,
     WHEN status  = 'DONE' THEN '거래완료'
 END) AS status
 FROM used_goods_board
-WHERE date_format(created_date, '%Y-%m-%d') = '2022-10-05'
+WHERE created_date = '2022-10-05'
 ORDER BY board_id DESC;
