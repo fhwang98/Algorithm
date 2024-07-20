c3924858408c45a771e80980e589a8671c92c61e
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2024-07-20 11:11:49 +0900

    [level 2] Title: 모음 사전, Time: 8.59 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,11 +1,15 @@
 import java.util.HashMap;
 
 public class Solution {
-    private static int idx = 0;
-    private static HashMap<String, Integer> dic = new HashMap<>();
-    private static String[] vowels = {"A", "E", "I", "O", "U"};
+    private static int idx;
+    private static HashMap<String, Integer> dic;
+    private static String[] vowels;
 
     public static int solution(String word) {
+        idx = 0;
+        dic = new HashMap<String, Integer>();
+        vowels = new String[] {"A", "E", "I", "O", "U"};
+        
         dfs("", 0);
         return dic.get(word);
     }
