d3364b91c0a87476fc247189c4404648af83c52d
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2024-07-25 22:11:42 +0900

    [level 2] Title: 피보나치 수, Time: 2.04 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,21 +1,14 @@
 class Solution {
     public int solution(int n) {
-        int answer = fibonacci(n) % 1234567;
-        return answer;
-    }
+        int answer = 0;
+        int f1 = 0, f2 = 1;
 
-    public int fibonacci(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-
-        int[] dp = new int[n + 1];
-        dp[0] = 0;
-        dp[1] = 1;
-
-        for (int i = 2; i <= n; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+        for(int i = 2; i<=n; i++){
+            answer = (f1 + f2) % 1234567;
+            f1 = f2;
+            f2 = answer;
         }
 
-        return dp[n];
+        return answer;
     }
 }
