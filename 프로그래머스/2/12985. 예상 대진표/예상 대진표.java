5b1d40cfc3bc82d9aca2eb2e98df3000f4b14fa5
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2024-07-29 16:37:11 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,18 +2,15 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        int answer = 1;
+        int answer = 0;
 
-        while (a > 0 && b > 0) {
-            if ((a - 1) / 2 == (b - 1) / 2) {
-                return answer;
-            }
-            a = a % 2 == 0 ? a / 2 : a / 2 + 1;
-            b = b % 2 == 0 ? b / 2 : b / 2 + 1;
+        while(a != b)
+        {
+            a = a/2 + a%2;
+            b = b/2 + b%2;
             answer++;
         }
         
-
         return answer;
     }
 }
