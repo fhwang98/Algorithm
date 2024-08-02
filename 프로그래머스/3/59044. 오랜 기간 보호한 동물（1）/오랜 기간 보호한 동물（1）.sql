e3d6b240b48db9ca82be21129c43890cb2605ccc
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2024-08-02 23:01:21 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,9 +1,12 @@
-SELECT name, datetime
+SELECT 
+    name, datetime
 FROM
-(SELECT i.*, RANK() OVER (ORDER BY i.datetime ASC) AS ranking
-FROM animal_ins i
-LEFT OUTER JOIN animal_outs o
-ON i.animal_id = o.animal_id
-WHERE o.datetime IS NULL 
-ORDER BY i.datetime ASC) AS sub
+    (SELECT 
+        i.*, 
+        RANK() OVER (ORDER BY i.datetime ASC) AS ranking
+    FROM 
+        animal_ins i
+    LEFT OUTER JOIN animal_outs o
+    ON i.animal_id = o.animal_id
+    WHERE o.datetime IS NULL ) AS sub
 WHERE sub.ranking BETWEEN 1 AND 3
