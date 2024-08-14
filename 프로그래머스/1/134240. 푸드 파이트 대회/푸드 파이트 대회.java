17e12674875e1f413260ab1a32b54caaa75b062b
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2024-08-14 12:08:21 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 2.45 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,6 +1,12 @@
 class Solution {
     public String solution(int[] food) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder("0");
+        for (int i = food.length - 1; i >= 0; i--) {
+            for (int j = 0; j < food[i] / 2; j++) {
+                answer.insert(0, i);
+                answer.append(i);
+            }
+        }
+        return answer.toString();
     }
 }
