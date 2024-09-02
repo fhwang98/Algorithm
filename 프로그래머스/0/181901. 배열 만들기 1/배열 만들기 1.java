68a32bf1c96a443ff88e9ad551351aa6bc294ad9
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2024-09-02 20:54:46 +0900

    [level 0] Title: 배열 만들기 1, Time: 13.82 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+class Solution {
+    public int[] solution(int n, int k) {
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % k == 0) answer.add(i);
+        }
+        return answer.stream().mapToInt(i->i).toArray();
+    }
+}
