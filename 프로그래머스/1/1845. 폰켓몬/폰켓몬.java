110cc800c542a1b94d2695715499259531645875
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2024-09-29 21:29:54 +0900

    [level 1] Title: 폰켓몬, Time: 3.79 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,17 +1,15 @@
+import java.util.Set;
 import java.util.HashSet;
 
 class Solution {
     public int solution(int[] nums) {
-        
-        int answer = 0;
-        HashSet<Integer> set = new HashSet<Integer>();
-        
+        //set의 길이 >= nums의 길이 / 2 -> nums의 길이 / 2만큼 고를 수 있다.
+        //set의 길이 < nums의 길이 / 2 -> set의 길이만큼 고를 수 있다.
+        Set<Integer> set = new HashSet<>();
         for (int n : nums) {
             set.add(n);
         }
-        
-        answer = set.size() >= nums.length / 2 ? nums.length / 2 : set.size();
-        
+        int answer = set.size() >= nums.length / 2 ? nums.length / 2 : set.size();
         return answer;
     }
 }
