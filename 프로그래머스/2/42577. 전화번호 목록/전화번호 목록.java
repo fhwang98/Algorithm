a50fa0754ca91022ea56fa854aaf16a7db571476
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2024-09-30 18:12:24 +0900

    [level 2] Title: 전화번호 목록, Time: 316.24 ms, Memory: 98.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,14 +2,11 @@ import java.util.Arrays;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
-        
+        if (phone_book.length == 1) return true;
         Arrays.sort(phone_book);
         for (int i = 1; i < phone_book.length; i++) {
             if (phone_book[i].startsWith(phone_book[i - 1])) return false;
-            
         }
-        
-        return answer;
+        return true;
     }
 }
