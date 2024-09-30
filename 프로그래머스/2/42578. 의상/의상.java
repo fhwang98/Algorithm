d022146d1d921cf9c0c74706ab66cb64035c97a5
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2024-09-30 18:19:05 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,19 +1,16 @@
+import java.util.Map;
 import java.util.HashMap;
 
 class Solution {
     public int solution(String[][] clothes) {
-        
+        Map<String, Integer> map = new HashMap<>();
+        for (String[] c : clothes) {
+            map.put(c[1], map.getOrDefault(c[1], 0) + 1);
+        }
         int answer = 1;
-        HashMap<String, Integer> map = new HashMap<String, Integer>();
-        
-        for (String[] item : clothes) {
-            map.put(item[1], map.getOrDefault(item[1], 0) + 1);
+        for (Integer v : map.values()) {
+            answer *= (v + 1);
         }
-        
-        for (String key : map.keySet()) {
-            answer *= map.get(key) + 1;
-        }
-
         return answer - 1;
     }
 }
