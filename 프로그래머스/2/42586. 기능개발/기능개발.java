88434f12753d847c6a134ec0fe9d9b47bf184074
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2024-10-01 15:33:57 +0900

    [level 2] Title: 기능개발, Time: 17.76 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,29 +1,22 @@
-import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Stream;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        
-        if (progresses.length == 1) return new int[]{1};
-        
-        ArrayList<Integer> answer = new ArrayList<Integer>();
+        List<Integer> answer = new ArrayList<>();
         int idx = 0;
-        int pub = 0;
-        
+        int count = 0;
         while (idx < progresses.length) {
-            
-            pub = 0;
-            
-            for (int i = idx; i < progresses.length; i++) {
+            count = 0;
+            for (int i = 0; i < progresses.length; i++) {
                 progresses[i] += speeds[i];
             }
-            
             while (idx < progresses.length && progresses[idx] >= 100) {
-                pub++;
+                count++;
                 idx++;
             }
-            
-            if (pub > 0) answer.add(pub);
-            
+            if (count > 0) answer.add(count);
         }
         return answer.stream().mapToInt(i->i).toArray();
     }
