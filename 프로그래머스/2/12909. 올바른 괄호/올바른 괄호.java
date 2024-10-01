841381f5578568483538542f251db96fcedd2865
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2024-10-01 15:50:05 +0900

    [level 2] Title: 올바른 괄호, Time: 18.57 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,15 +3,17 @@ import java.util.Stack;
 class Solution {
     boolean solution(String s) {
         if (s.startsWith(")") || s.length() % 2 == 1) return false;
-        boolean answer = true;
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
             char cur = s.charAt(i);
-            if (stack.isEmpty() && cur == ')') return false;
-            if (!stack.isEmpty() && cur == ')') stack.pop();
-            else if (cur == '(') stack.push(cur);
+            if (cur == ')') {
+                if (stack.isEmpty()) return false;    
+                else stack.pop();
+            } else {
+                stack.push(cur);
+            }
         }
         if (!stack.isEmpty()) return false;
-        return answer;
+        return true;
     }
 }
