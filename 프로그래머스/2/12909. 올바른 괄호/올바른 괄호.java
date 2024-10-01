6ee05185ee9efc1eb2aa5d5ead0fae23afb6df36
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2024-10-01 15:46:20 +0900

    [level 2] Title: 올바른 괄호, Time: 23.07 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,27 +2,16 @@ import java.util.Stack;
 
 class Solution {
     boolean solution(String s) {
-        
-        boolean answer = false;
-        
-        if (s.length() % 2 == 1 ||
-            s.startsWith(")") || s.endsWith("(")) return answer;
-        
-        String[] arr = s.split("");
-        int cnt = 0;
-        
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].equals("(")) {
-                cnt++;
-            } else if (cnt < 1) {
-                return answer;
-            } else {
-                cnt--;
-            }
+        if (s.startsWith(")") || s.length() % 2 == 1) return false;
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            if (stack.isEmpty() && cur == ')') return false;
+            if (!stack.isEmpty() && cur == ')') stack.pop();
+            else if (cur == '(') stack.push(cur);
         }
-        
-        if (cnt == 0) answer = true;
-
+        if (!stack.isEmpty()) return false;
         return answer;
     }
 }
