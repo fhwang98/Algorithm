efc3a8476de846c7a392e89a6ffe8d2e5ebf0e5b
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2024-10-02 23:34:04 +0900

    [level 2] Title: 프로세스, Time: 2.65 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,53 +1,38 @@
+import java.util.List;
 import java.util.ArrayList;
 import java.util.stream.Stream;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        
-        int order = 1;
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        int pub = 0;
-        
+        if (priorities.length == 1) return 1;
+        List<Integer> list = new ArrayList<>();
         for (int p : priorities) {
             list.add(p);
         }
-        
-        //리스트가 빌 때까지 반복
-        while (!list.isEmpty()) {
-            
-            int cur = list.get(0);
-            
-            //뒤에 우선순위가 높은 프로세스가 있는지 확인
-            if (list.stream().anyMatch(n -> n > cur)) {
-                
-                //있으면 맨 뒷 순서로 이동
+        int answer = 1;
+        while (list.size() > 0) {
+            // 맨 앞의 숫자가 실행될 수 없음
+            if (list.get(0) < getHighest(list)) {
+                // 맨 뒷 순서로 이동
                 list.add(list.remove(0));
-                
-                //location이 맨 앞이었으면 맨 뒤로 이동
-                if (location == 0) {
-                    location = list.size() - 1;
-                } else { // 아니면 하나 줄여줌
-                    location--;
-                }
-                
+                // location 같이 이동
+                if (location == 0) location = list.size() - 1;
+                else location--;
             } else {
-                //지금 우선순위가 가장 높다
-                
-                //지금 실행할 차례다
-                if (location == 0) {
-                    //순서를 반환
-                    return order;
-                }
-                
-                //실행됐으면 리스트에서 삭제 & 순서++
-                //location도 줄여줌
+                // 맨 앞의 숫자가 실행될 차례
+                if (location == 0) break;
+                else location--;
                 list.remove(0);
-                order++;
-                location--;
-
+                answer++;
             }
         }
-        
-        return order;
+        return answer;
+    }
+    public int getHighest(List<Integer> list) {
+        int max = 0;
+        for (int i = 0; i < list.size(); i++) {
+            max = Math.max(max, list.get(i));
+        }
+        return max;
     }
 }
