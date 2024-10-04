bdea4e67ad5c5b9c4d3e19eb31636ddc4c6b38c6
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2024-10-04 14:35:23 +0900

    [level 2] Title: 더 맵게, Time: 1725.11 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -2,28 +2,17 @@ import java.util.PriorityQueue;
 
 class Solution {
     public int solution(int[] scoville, int K) {
-        int answer = 0;
-        
-        // 우선순위 낮은것 = 작은 숫자부터 나옴
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         for (int s : scoville) {
             pq.add(s);
         }
-
-        //더이상 섞을 수 없을 때까지 반복
+        int answer = 0;
         while (pq.size() > 1) {
-            
-            //모든 음식의 스코빌이 K 이상 > 섞은 횟수 반환
-            if (pq.peek() >= K) return answer;
-            
-            //섞는다
+            if (pq.peek() >= K) break;
             pq.add(pq.poll() + pq.poll() * 2);
-            //섞은 횟수 증가
             answer++;
-            
         }
-        
-        // 꺼냈을때 K 이상이면 섞은 횟수, 아니면 -1 반환
-        return pq.peek() >= K ? answer : -1;
+        if (pq.peek() < K) answer = -1;
+        return answer;
     }
 }
