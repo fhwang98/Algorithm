5332832333546a8ab5b7549bb528fe6c58ee7521
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2024-10-13 23:55:37 +0900

    [level 1] Title: 체육복, Time: 0.04 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,29 +2,35 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        //students 배열 안에 체육복의 개수 저장
         int[] students = new int[n];
-        Arrays.fill(students, 1);
+
+        for (int i = 0; i < lost.length; i++) {
+            students[lost[i] - 1]--;
+        }
         
-        for (int l : lost) students[l - 1]--;
-        for (int r : reserve) students[r - 1]++;
-        
+        for (int i = 0; i < reserve.length; i++) {
+            students[reserve[i] - 1]++;
+        }
+
         for (int i = 0; i < n; i++) {
-            if (students[i] == 0) {
-                if (i != 0 && students[i - 1] == 2) {
-                    students[i]++;
+            if (students[i] == -1) { // 도난당했다
+                if (i != 0 && students[i - 1] == 1) { // 앞사람이 두개있다
                     students[i - 1]--;
-                } else if (i != n - 1 && students[i + 1] == 2) {
                     students[i]++;
+                } else if (i != n - 1 && students[i + 1] == 1) { // 뒷사람이 두개있다
                     students[i + 1]--;
+                    students[i]++;
                 }
             }
         }
         
-        int answer = 0;
-        for (int s : students) {
-            if (s > 0) answer++;
+        for (int no : students) {
+            if (no > -1) {
+                answer++;
+            }
         }
-        
         return answer;
     }
 }
