77c82fa04db5b31021bf329c2af56277cbe56c09
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2024-10-22 14:40:12 +0900

    [level 3] Title: 업그레이드 할 수 없는 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/273712. 업그레이드 할 수 없는 아이템 구하기/업그레이드 할 수 없는 아이템 구하기.sql
@@ -0,0 +1,5 @@
+-- 누군가의 parent가 아닌 아이템
+SELECT item_id, item_name, rarity
+FROM item_info
+WHERE item_id NOT IN (SELECT parent_item_id FROM item_tree WHERE parent_item_id IS NOT NULL)
+ORDER BY item_id DESC
