42cff744fe53a8831a60186914dd9f7deda95535
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2025-12-30 15:22:18 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -1,4 +1,4 @@
-SELECT id, email, first_name, last_name
-FROM developer_infos
+SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
+FROM DEVELOPER_INFOS
 WHERE SKILL_1 = 'Python' OR SKILL_2 = 'Python' OR SKILL_3 = 'Python'
-ORDER BY id ASC
+ORDER BY ID ASC;
