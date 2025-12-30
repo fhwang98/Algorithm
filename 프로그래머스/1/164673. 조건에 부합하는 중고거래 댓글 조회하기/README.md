a5d3a398a26af958d3439f3167120d2fc46eb3dd
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2025-12-30 13:58:39 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/README.md
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/README.md
@@ -1,6 +1,6 @@
 # [level 1] 조건에 부합하는 중고거래 댓글 조회하기 - 164673 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/164673?language=mysql) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/164673) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 07월 15일 17:27:41
+2025년 12월 30일 13:58:37
 
 ### 문제 설명
 
--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,14 +1,12 @@
--- 코드를 입력하세요
-SELECT
-    b.title,
-    b.board_id,
-    r.reply_id,
-    r.writer_id,
-    r.contents,
-    date_format(r.created_date, '%Y-%m-%d') AS created_date
-FROM used_goods_board b
-INNER JOIN used_goods_reply r
+SELECT 
+    b.TITLE,
+    b.BOARD_ID,
+    r.REPLY_ID,
+    r.WRITER_ID,
+    r.CONTENTS,
+    DATE_FORMAT(r.created_date, '%Y-%m-%d') as CREATED_DATE
+FROM USED_GOODS_BOARD as b
+INNER JOIN USED_GOODS_REPLY as r
 ON b.board_id = r.board_id
-WHERE b.created_date LIKE '2022-10%'
-ORDER BY r.created_date ASC,
-        b.title ASC
+WHERE b.CREATED_DATE LIKE '2022-10-%'
+ORDER BY r.created_date ASC, b.title ASC;
