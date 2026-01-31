570e0525db40ebc799261b93f400239838b94407
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2026-01-31 15:44:00 +0900

    [level 2] Title: 조이스틱, Time: 0.08 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,26 +1,39 @@
 class Solution {
     public int solution(String name) {
         
-        int answer = 0;
-        int length = name.length();
-        int endA = 0; 
-        int move = length - 1; 
-
-        for(int i = 0; i < length; i++){
-            
-            // 상하 이동 횟수 중 작은 횟수
-            answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
-
-            // 연속된 마지막 A 찾기
-            endA = i + 1;
-            while(endA < length && name.charAt(endA) == 'A'){
-                endA++;
+        // 상하이동 빠른것
+        // A BCDEFGHIJKLM 12 N 12 OPQRSTUVWXYZ
+        // B ~ M 아래로 이동
+        // N은 상관없이 13
+        // O ~ P 는 위로 이동
+        int upDown = 0;
+        for (int i = 0; i < name.length(); i++) {
+            char c = name.charAt(i);
+            if (c > 'A' && c <= 'N') {
+                upDown += c - 'A';
+            } else if (c > 'N' && c <= 'Z') {
+                upDown += 'Z' - c + 1;
             }
-
-            // 좌우 이동 횟수 중 작은 것
-            move = Math.min(move, i * 2 + (length - endA));// 순서대로 가기
-            move = Math.min(move, i + (length - endA) * 2);// 뒤로 돌아 가기
         }
-        return answer + move;
+        
+        // 좌우이동 빠른것을 먼저 찾는다
+        // 연속된 A를 통과 -> 개손해
+        int leftRight = name.length() - 1;
+        for (int i = 0; i < name.length() - 1; i++) {
+            if (name.charAt(i + 1) != 'A') continue;
+            // 다음에 만나는 A가 아닌 글자의 위치
+            int nextIndex = i + 1; 
+            while (nextIndex < name.length() && name.charAt(nextIndex) == 'A') {
+                nextIndex++;
+            }
+            // 경우1. 0 ~ i 이동 + i ~ 0 이동 + 0 ~ nextIndex 뒤로 이동
+            int right = 2 * i + name.length() - nextIndex;
+            // 경우2. 0 ~ nextIndex 이동 + nextIndex ~ 0 이동 + 0 ~ i 이동
+            int left = (name.length() - nextIndex) * 2 + i;
+            
+            leftRight = Math.min(leftRight, Math.min(left, right));
+        }
+        
+        return upDown + leftRight;
     }
 }
