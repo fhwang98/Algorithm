130d29ec8f25e19c89dbd68f2071dde92e59f90d
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2026-02-03 12:16:37 +0900

    [level 3] Title: 정수 삼각형, Time: 12.30 ms, Memory: 64.5 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,31 +1,23 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[][] triangle) {
-    
         int answer = 0;
+        // 삼각형 꼭대기에서 바닥까지 거쳐간 숫자의 합이 가장 큰 경우
+        // 아래로 내려갈 때 대각선 양쪽 한칸만 이동 가능
+        // triangle[i][j] 에 도착하는 경로
+        // triangle[i-1][j] 에서 오거나 triangle[i-1][j-1]에서 오는경우
+        // dp[i][j]는 dp[i-1][j] dp[i-1][j-1] 중 큰 값 + triangle[i][j]
         int[][] dp = new int[triangle.length][triangle.length];
-        // triangle[i][j] 칸에 도착하는 경로
-        // [i-1][j-1] 또는 [i-1][j]에서 오는 경우
-        // 현재 경로까지 가능한 최대값
         dp[0][0] = triangle[0][0];
         for (int i = 1; i < triangle.length; i++) {
-            for (int j = 0; j < triangle[i].length; j++) {
-                int temp = triangle[i][j];
-                // j가 0이면 dp[i-1][0] + 현재값
+            for (int j = 0; j <= i; j++) {
                 if (j == 0) {
-                    temp += dp[i-1][0];
-                // j == i면 dp[i-1][j-1] + 현재값
+                    dp[i][j] = dp[i-1][j] + triangle[i][j];
                 } else if (j == i) {
-                    temp += dp[i-1][j-1];
-                }
-                // 중간이면 dp[i-1][j-1]와 dp[i-1][j] 중 큰 값 + 현재값
-                else {
-                    temp += Math.max(dp[i-1][j-1], dp[i-1][j]);
-                }
-                dp[i][j] = temp;
-                
-                answer = Math.max(answer, temp);
+                    dp[i][j] = dp[i-1][j-1] + triangle[i][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-1]) + triangle[i][j];
+                }   
+                answer = Math.max(answer, dp[i][j]);
             }
         }
         return answer;
