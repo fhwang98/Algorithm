d58091d3ac743290b2290a93546ac63a3645207d
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2026-02-05 12:51:46 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 120 ms, Memory: 15072 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -3,9 +3,6 @@ import java.util.*;
 
 public class Main {
 
-    static String[] white = {"WBWBWBWB", "BWBWBWBW"};
-    static String[] black = {"BWBWBWBW", "WBWBWBWB"};
-
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,8 +18,8 @@ public class Main {
         int min = Integer.MAX_VALUE;
         for (int i = 0; i <= N - 8; i++) {
             for (int j = 0; j <= M - 8; j++) {
-                int w = count(board, i, j, white);
-                int b = count(board, i, j, black);
+                int w = count(board, i, j, 'W');
+                int b = count(board, i, j, 'B');
 
                 min = Math.min(min, Math.min(w, b));
             }
@@ -36,13 +33,13 @@ public class Main {
         br.close();
     }
 
-    private static int count(String[] board, int row, int col, String[] pattern) {
+    private static int count(String[] board, int row, int col, char start) {
         int count = 0;
+        char other = start == 'W' ? 'B' : 'W';
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                if (board[row + i].charAt(col + j) != pattern[i % 2].charAt(j)) {
-                    count++;
-                }
+                char expected = ((i + j) % 2 == 0) ? start : other;
+                if (board[row + i].charAt(col + j) != expected) count++;
             }
         }
         return count;
