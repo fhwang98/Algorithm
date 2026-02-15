f70e49e8cc377a1422ed1961e09f98f5a047bb01
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2026-02-15 10:43:11 +0900

    [Silver IV] Title: 스택, Time: 156 ms, Memory: 19732 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,47 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+    import java.io.*;
+    import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+    public class Main {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int count = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
+        public static void main(String[] args) throws Exception {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        Stack<Integer> stack = new Stack<>();
-
-        for (int i = 0; i < count; i++) {
-            String[] line = br.readLine().split(" ");
-
-            switch (line[0]) {
-                case "push":
-                    stack.push(Integer.parseInt(line[1]));
-                    break;
-                case "pop":
-                    sb.append(stack.isEmpty()? "-1" : stack.pop());
-                    sb.append("\n");
-                    break;
-                case "size":
-                    sb.append(stack.size());
-                    sb.append("\n");
-                    break;
-                case "empty":
-                    sb.append(stack.isEmpty()? "1" : "0");
-                    sb.append("\n");
-                    break;
-                case "top":
-                    sb.append(stack.isEmpty()? "-1" : stack.peek());
-                    sb.append("\n");
-                    break;
-                default:
-                    break;
+            int N = Integer.parseInt(br.readLine());
+            Stack<Integer> stack = new Stack<>();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                switch (st.nextToken()) {
+                    case "push":
+                        stack.push(Integer.parseInt(st.nextToken()));
+                        break;
+                    case "pop":
+                        sb.append(stack.isEmpty() ? -1 : stack.pop()).append("\n");
+                        break;
+                    case "size":
+                        sb.append(stack.size()).append("\n");
+                        break;
+                    case "empty":
+                        sb.append(stack.isEmpty() ? 1 : 0).append("\n");
+                        break;
+                    case "top":
+                        sb.append(stack.isEmpty() ? -1 : stack.peek()).append("\n");
+                        break;
+                    default:
+                        break;
+                }
             }
-
+            bw.write(sb.toString());
+            bw.flush();
+            bw.close();
+            br.close();
         }
-        br.close();
-        System.out.println(sb.toString());
-
     }
-}
+
