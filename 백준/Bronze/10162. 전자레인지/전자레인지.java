c39cefbd819cdce559464d00aa4a563c5bf48516
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2026-02-18 22:30:35 +0900

    [Bronze III] Title: 전자레인지, Time: 132 ms, Memory: 16376 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10162. 전자레인지/전자레인지.java
+++ 백준/Bronze/10162. 전자레인지/전자레인지.java
@@ -12,15 +12,14 @@ public class Main {
         // 만들 수 없으면 -1
         int a = T / 300;
         int b = (T - a * 300) / 60;
-        int c = (T - b * 60) / 10;
+        int c = (T - a * 300 - b * 60) / 10;
 
         if (a * 300 + b * 60 + c * 10 != T) {
             bw.write("-1");
         } else {
             bw.write(a + " " + b + " " + c + "\n");
         }
-
-
+        
         bw.flush();
         bw.close();
         br.close();
