19422d5cea3935d4dd85b6ce8d614ced4c6a0b7e
fhwang98 <59570517+fhwang98@users.noreply.github.com>
2026-02-18 22:26:55 +0900

    [Bronze III] Title: 전자레인지, Time: 128 ms, Memory: 16160 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/10162. 전자레인지/전자레인지.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T  = Integer.parseInt(br.readLine());
+        // 300초, 60초, 10초 로 T 만드는 최소 개수
+        // 만들 수 없으면 -1
+        int a = T / 300;
+        int b = (T - a * 300) / 60;
+        int c = (T - b * 60) / 10;
+
+        if (a * 300 + b * 60 + c * 10 != T) {
+            bw.write("-1");
+        } else {
+            bw.write(a + " " + b + " " + c + "\n");
+        }
+
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+
+}
